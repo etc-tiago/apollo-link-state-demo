6b10298dfb529dd4369177ac6cba136a7e9845e2
Tiago Souza <tiago@explorethecuriosity.com>
2020-04-28 13:02:50 -0300

    Apply apollo provider configuration

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
 import React from "react";
 import { Grid, Segment, Container } from "semantic-ui-react";
+import ApolloClient, { InMemoryCache } from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 
 import { ItemsForPurchase } from "./components/ItemsForPurchase";
 import { UserCart } from "./components/UserCart";
+import { available_items } from "./api";
+
+const cache = new InMemoryCache({});
+
+const client = new ApolloClient({
+  cache,
+  clientState: {
+    defaults: {
+      cart: {
+        items: [],
+        total: 0,
+        __typename: "Cart",
+      },
+      currency: "USD",
+      itemsForSale: available_items,
+    },
+  },
+});
 
 export function App() {
   return (
-    <Container>
-      <br />
-      <Grid>
-        <Grid.Row columns="one">
-          <Grid.Column>
-            <Segment>
-              <h2>Apollo Local State Management Store</h2>
-            </Segment>
-          </Grid.Column>
-        </Grid.Row>
+    <ApolloProvider client={client}>
+      <Container>
+        <br />
+        <Grid>
+          <Grid.Row columns="one">
+            <Grid.Column>
+              <Segment>
+                <h2>Apollo Local State Management Store</h2>
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
 
-        <Grid.Row columns="two">
-          <Grid.Column width="eleven">
-            <ItemsForPurchase />
-          </Grid.Column>
-          <Grid.Column width="five">
-            <UserCart />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Container>
+          <Grid.Row columns="two">
+            <Grid.Column width="eleven">
+              <ItemsForPurchase />
+            </Grid.Column>
+            <Grid.Column width="five">
+              <UserCart />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    </ApolloProvider>
   );
 }
