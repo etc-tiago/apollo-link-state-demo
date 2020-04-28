0c752c56933eaae7c9131e30f5f9781929d62e0a
Tiago Souza <tiago@explorethecuriosity.com>
2020-04-28 14:00:51 -0300

    Added currency convert

--- src/components/CurrencyButtons.js
+++ src/components/CurrencyButtons.js
@@ -1,21 +1,31 @@
-import React from 'react'
-import { Button } from 'semantic-ui-react'
+import React from "react";
+import { Button } from "semantic-ui-react";
+import { useMutation } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
 
+const MUTATION_CONVERT_CURRENTY = gql`
+  mutation($newCurrency: String!) {
+    convertCurrency(newCurrency: $newCurrency) @client
+  }
+`;
 
 export function CurrencyButtons(props) {
+  const [convertCurrency] = useMutation(MUTATION_CONVERT_CURRENTY);
 
   return (
     <Button.Group fluid>
       <Button
-        content='USD'
-        icon='usd'
-        positive={props.currency === 'USD'}
+        content="USD"
+        icon="usd"
+        positive={props.currency === "USD"}
+        onClick={() => convertCurrency({ variables: { newCurrency: "USD" } })}
       />
       <Button
-        content='EUR'
-        icon='euro'
-        positive={props.currency === 'EUR'}
+        content="EUR"
+        icon="euro"
+        positive={props.currency === "EUR"}
+        onClick={() => convertCurrency({ variables: { newCurrency: "EUR" } })}
       />
     </Button.Group>
-  )
+  );
 }
--- src/resolvers.js
+++ src/resolvers.js
@@ -1,6 +1,8 @@
 import { QUERY_CART_INFO } from "./components/UserCart";
 import { QUERY_AVAILABLE_ITEMS } from "./components/ItemsForPurchase";
 
+import { convertPrice } from "./util";
+
 export const resolvers = {
   Mutation: {
     addItemToCart: (_, args, { cache }) => {
@@ -23,6 +25,36 @@ export const resolvers = {
       });
 
       return newItem;
+    },
+    async convertCurrency(_, { newCurrency }, { cache }) {
+      const {
+        currency,
+        cart: { total },
+      } = cache.readQuery({ query: QUERY_CART_INFO });
+
+      const { itemsForSale } = cache.readQuery({
+        query: QUERY_AVAILABLE_ITEMS,
+      });
+
+      const itemsWithConvertedPricing = await Promise.all(
+        itemsForSale.map(async (item) => ({
+          ...item,
+          price: await convertPrice(currency, newCurrency, item.price),
+        }))
+      );
+
+      cache.writeData({
+        data: {
+          itemsForSale: itemsWithConvertedPricing,
+          cart: {
+            total: await convertPrice(currency, newCurrency, total),
+            __typename: "Cart",
+          },
+          currency: newCurrency,
+        },
+      });
+
+      return newCurrency;
     },
   },
 };
